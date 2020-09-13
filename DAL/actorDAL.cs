55087a70f17c3404737830c2496789f13b334db5
marileidy <marileidymanzueta@gmail.com>
2020-09-13 17:52:25 -0400

    add DAL actor and movie

---
+++ DAL/actorDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using movie.BLL;
+
+namespace movie.DAL
+{
+    class actorDAL
+    {
+        conexionDALL conexion;
+        public actorDAL()
+        {
+            conexion = new conexionDALL();
+        }
+
+        public bool ADD(actorBLL oactorBLL)
+        {
+            return conexion.Command("INSERT INTO actorTable (name) VALUES ('"+ oactorBLL.Name+ "');");
+        }
+
+    }
+}
